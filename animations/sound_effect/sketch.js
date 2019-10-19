2da51286e5396e9134891f562152eaa9610ad883
Sebastien Girard <sebastiengrd@gmail.com>
2019-10-19 14:37:53 -0400

    added noise to the audio

--- animations/sound_effect/sketch.js
+++ animations/sound_effect/sketch.js
@@ -1,6 +1,11 @@
 let mic, fft;
 var validate = true;
 
+var inc = 0.01;
+var time = 0;
+
+var bar_width = 10;
+
 function setup() {
   createCanvas(710, 400);
   noFill();
@@ -21,11 +26,27 @@ function draw() {
     console.log(spectrum)
   } 
   beginShape();
-  for (i = 0;i < spectrum.length; i += 10){
+  for (i = 0;i < spectrum.length; i += 1){
     let new_height = map(spectrum[i], 0, 255, height, 0);
     vertex(i, new_height);
     stroke(color(255, 0, 0));
     line(i, height, i, new_height);
   }
   endShape();
+  var yoff = 1;
+  loadPixels();
+  for (var y = 0; y < height; y++) {
+    var xoff = 1;
+    for (var x = 0; x < width; x++) {
+      var index = (x + y * width) * 4;
+      var r = noise(xoff, yoff, time) * 255;
+      pixels[index + 0] *= r/255;
+      pixels[index + 1] *= r/255;
+      pixels[index + 2] *= r/255;
+      xoff += inc;
+    }
+    yoff += inc;
+  }
+  time += 0.1;
+  updatePixels();
 }
