c35e78f3b27b20e57ced7de90dbf6efc239fac38
Sebastien Girard <sebastiengrd@gmail.com>
2019-10-20 17:30:04 -0400

    auto adjustment for width and height

--- animations/sound_effect/sketch.js
+++ animations/sound_effect/sketch.js
@@ -4,14 +4,15 @@ var index = 0;
 var time = 0;
 var inc = 0.01;
 
-var wanted_width = 0;
+var wanted_width = 1800;
 var wanted_height = 150;
 
-var bar_width = 10;
+var data_resolution = 2;
+
 
 
 function setup() {
-	createCanvas(data[0].length, wanted_height);
+	createCanvas(wanted_width, wanted_height);
   myLoop();
 
 
@@ -23,7 +24,7 @@ function myLoop () {
 
     // get the spectrum
     if (index >= data.length){index = 0;} //reset the animation at the end
-    let spectrum = data[index];
+    var spectrum = data[index];
     index++;
     
     // initialize things
@@ -34,18 +35,26 @@ function myLoop () {
     let yoff = 1;
     loadPixels();
 
-    for (let x = 0; x < width; x += bar_width) {
-    	
+    for (let x_data = 0; x_data < data.length; x_data += data_resolution) {
+    	var min_bef = 0;
+    	var max_bef = data.length - 40;
+    	var min_aft = 0;
+    	var max_aft = wanted_width;
+    	var new_x = Math.round(map(x_data, min_bef, max_bef, min_aft, max_aft));
+    	// console.log(x);
+    	// var new_x = 5;
+
       let xoff = 1;
-      let bar_height = map(spectrum[x], -150, 50, height, 0);
-      let col = color(map(x, 0, spectrum.length, 0, 235), 255, 255);
+      let bar_height = map(Math.round(spectrum[x_data]), -150, 50, wanted_height, 0);
+      let col = color(map(new_x, 0, wanted_width, 0, 235), 255, 255);
       let r = red(col);
       let g = green(col);
       let b = blue(col)
 
+
       for (let y = 0; y < bar_height; y++){
         let inverted_y = height - y;
-        let index = (x + inverted_y * width) * 4;
+        let index = (new_x + inverted_y * wanted_width) * 4;
         // noiseDetail(8, 0.65)
         let n = noise(xoff, yoff, time);
         // n = 0.9;
