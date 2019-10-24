7fb2f571ea84cdd89b7e2a53355159b40625f592
Sebastien Girard <sebastiengrd@gmail.com>
2019-10-23 23:24:34 -0400

    commit

--- scripts/sketch_music_visualisation.js
+++ scripts/sketch_music_visualisation.js
@@ -20,7 +20,7 @@ function setup() {
 
 }
 
-function myLoop () {
+function myLoop () {           
 	setTimeout(function () {
    // Code is here
 
@@ -32,14 +32,12 @@ function myLoop () {
     // initialize things
     background('rgba(58, 58, 58, 0.1)');
     colorMode(HSB, 255, 255, 255);
-
   
 
     let yoff = 1;
     loadPixels();
 
     for (let x_data = 0; x_data < data.length; x_data += data_resolution) {
-    	if 
     	var min_bef = 0;
     	var max_bef = data.length - 40;
     	var min_aft = 0;
@@ -49,28 +47,26 @@ function myLoop () {
     	// var new_x = 5;
 
       let xoff = 1;
-      let bar_height = map(Math.round(spectrum[x_data]), -50, 60, wanted_height, 0);
+      let bar_height = map(Math.round(spectrum[x_data]), -150, 50, wanted_height, 0);
       let col = color(map(new_x, 0, wanted_width, 0, 235), 255, 255);
       let r = red(col);
       let g = green(col);
       let b = blue(col)
 
 
-      for (let y = height; y > bar_height; y--){
-        // let inverted_y = height - y;
-        let index = (new_x + 7 + y * wanted_width) * 4;
+      for (let y = 0; y < bar_height; y++){
+        let inverted_y = height - y;
+        let index = (new_x + 7 + inverted_y * wanted_width) * 4;
         // noiseDetail(8, 0.65)
         let n = noise(xoff, yoff, time);
         // n = 0.9;
         pixels[index + 0] = r * (n+0.1);
         pixels[index + 1] = g * (n+0.1);
         pixels[index + 2] = b * (n+0.1);
-        // pixels[index + 3] = 0
         xoff += inc;
       }
     	yoff += inc;
-  	}
-
+  	} 
     time += 0.1;
     updatePixels();
 
@@ -85,3 +81,4 @@ function resize(){
 	wanted_height = 150;
 	resizeCanvas(wanted_width, wanted_height, true)
 }
+
