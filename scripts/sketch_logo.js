66e4344ab57588a1fd9a5162c165096474516596
Sebastien Girard <sebastiengrd@gmail.com>
2019-10-31 23:45:38 -0400

    added interactive logo to index

--- scripts/sketch_logo.js
+++ scripts/sketch_logo.js
@@ -1,30 +1,36 @@
-let noiseScale=0.02;
-var xoff = 0;
-var count = 0;
-function setup() {
-  let cnv = createCanvas(90, 90);
-  cnv.parent("logo-animation");
-  img = loadImage('logo_transparent.png'); // Load the image
-  pixelDensity(1);
-}
+const s1 = ( sketch ) => {
+  let noiseScale=0.1;
+  let xoff = 0;
+  let count = 0;
+  let w = 90;
+  let h = 90;
+  let pxDensity = 5;
 
-function draw() {
-  background("rgba(255, 255, 255, 0)");
+  sketch.setup = () => {
+    sketch.createCanvas(w, h);
+    // img = sketch.loadImage('logo_transparent.png'); // Load the image
+    sketch.pixelDensity(5);
+  }
 
-  let img1 = createImage(90, 90);
-  img1.loadPixels();
-  for (let i = 0; i < img1.width; i++) {
-    for (let j = 0; j < img1.height; j++) {
-      if (((i-45)**2 + (j-45)**2)**0.5 < 30){
-        let noiseVal = noise((count+i)*noiseScale) * 255;
-        stroke(noiseVal*255);
-        img1.set(i, j, color((noiseVal)*0.3, (noiseVal)*0.6, (noiseVal)*0.2));
+  sketch.draw = () => {
+    sketch.background("rgba(255, 255, 255, 0)");
+
+    let img1 = sketch.createImage(w, h);
+    img1.loadPixels();
+    for (let i = 0; i < img1.width; i++) {
+      for (let j = 0; j < img1.height; j++) {
+        if (((i-(w/2))**2 + (j-(h/2))**2)**0.5 < w/4.2){
+          let noiseVal = sketch.noise((count+i)*noiseScale) * 255;
+          img1.set(i, j, sketch.color((noiseVal)*0.65, (noiseVal)*0.3, 50));
+        }
       }
     }
+    count = count-1.2;
+    img1.updatePixels();
+    sketch.image(img1, 0, 0);
+    // sketch.image(img, -23, -17, 130, 130);
+    
   }
-  count--;
-  img1.updatePixels();
-  image(img1, 0, 0);
-  image(img, -23, -17, 130, 130);
-  
 }
+
+logo_p5 = new p5(s1, document.getElementById("div_sketch_logo"));
